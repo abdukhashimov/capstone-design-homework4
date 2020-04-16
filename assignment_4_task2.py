16f657352700299af05ed9780f2899cfa3c17f6e
Madiyor <abdukhashimov@yandex.ru>
2020-04-16 16:05:46 +0500

    refactored the task2

--- assignment_4_task2.py
+++ assignment_4_task2.py
@@ -10,5 +10,15 @@ import cv2
 
 image = cv2.imread('images/moonphcamera.jpg')
 
-cv2.imwrite('results/task2/people.jpeg', image[80:310, 72:210])
-cv2.imwrite('results/task2/cameras.jpeg', image[120:335, 257:494])
+people = image[80:310, 72:210]
+cameras = image[120:335, 257:494]
+
+# saving results
+cv2.imwrite('results/task2/people.jpeg', people)
+cv2.imwrite('results/task2/cameras.jpeg', cameras)
+# end of saving results
+
+# displaying results
+cv2.imshow('people.jpeg', people)
+cv2.imshow('cameras.jpeg', cameras)
+cv2.waitKey(5000)
