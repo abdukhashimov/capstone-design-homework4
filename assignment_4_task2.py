bc84aef29dacbf63b650a00afb15af58594514cb
Madiyor <abdukhashimov@yandex.ru>
2020-04-17 20:03:04 +0500

    Added comments to the task2

--- assignment_4_task2.py
+++ assignment_4_task2.py
@@ -5,15 +5,21 @@ This script manually separates moonphcamera.jpg image into two different images
 ID: U1610131
 Name: Madiyor Abdukhashimov
 """
-
+# importing opencv library - to be able to import opencv you need to install
+# it using pip. The process of installation is provided in [README.md](../README.md)
 import cv2
 
+
+# read the image from the `images/ directory`
 image = cv2.imread('images/moonphcamera.jpg')
 
+# selecting the people from the image and cutting then saving it
 people = image[80:310, 72:210]
+
+# selecting the cameras from the image and cutting then saving it
 cameras = image[120:335, 257:494]
 
-# saving results
+# saving results the the `results` directory
 cv2.imwrite('results/task2/people.jpeg', people)
 cv2.imwrite('results/task2/cameras.jpeg', cameras)
 # end of saving results
@@ -21,4 +27,7 @@ cv2.imwrite('results/task2/cameras.jpeg', cameras)
 # displaying results
 cv2.imshow('people.jpeg', people)
 cv2.imshow('cameras.jpeg', cameras)
+# end of displaying
+
+# wait 5 seconds
 cv2.waitKey(5000)
