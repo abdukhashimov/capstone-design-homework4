a5501f8caeb1960fb453aa84ab04c93da1e4f61d
Madiyor <abdukhashimov@yandex.ru>
2020-04-17 20:16:38 +0500

    removed extra comments to improve readiblity

--- assignment_4_task2.py
+++ assignment_4_task2.py
@@ -5,8 +5,7 @@ This script manually separates moonphcamera.jpg image into two different images
 ID: U1610131
 Name: Madiyor Abdukhashimov
 """
-# importing opencv library - to be able to import opencv you need to install
-# it using pip. The process of installation is provided in [README.md](../README.md)
+
 import cv2
 
 
