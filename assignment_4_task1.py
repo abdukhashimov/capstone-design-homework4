833ffcc8d589168fb65e9a95cc4490ed36df13e3
Madiyor <abdukhashimov@yandex.ru>
2020-04-17 20:16:04 +0500

    removed the comments to improve the readablity

--- assignment_4_task1.py
+++ assignment_4_task1.py
@@ -5,30 +5,27 @@ ID: U1610131
 Name: Madiyor Abdukhashimov
 """
 
-# importing opencv library - to be able to import opencv you need to install
-# it using pip. The process of installation is provided in [README.md](../README.md)
 import cv2
 
 
-# read the image from the `images/ directory`
 image = cv2.imread('images/moonphcamera.jpg')
 
 # place the red rectangle onto people.
 people_and_moon = cv2.rectangle(
-    img=image,  # the image that we have loaded
-    pt1=(72, 80),  # starting point for rectangle
-    pt2=(208, 307),  # ending point for rectangle
-    color=(0, 0, 255),  # the red color that we are sending
-    thickness=3,  # the size for the line of rectangle
+    img=image,
+    pt1=(72, 80),
+    pt2=(208, 307),
+    color=(0, 0, 255),
+    thickness=3,
 )
 
 # place the rectangle onto the Moon
 people_and_moon = cv2.rectangle(
-    img=people_and_moon,  # the image that has moon and people
-    pt1=(392, 126),  # the starting point for rectangle
-    pt2=(432, 160),  # the ending poin for rectangle
-    color=(0, 0, 0),  # the color of rectangle
-    thickness=1,  # the size for the line of rectangle
+    img=people_and_moon,
+    pt1=(392, 126),
+    pt2=(432, 160),
+    color=(0, 0, 0),
+    thickness=1,
 )
 
 
