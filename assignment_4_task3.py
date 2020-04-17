aaa0fa62bba234e1685d653ae55f2da82e3ccb3b
Madiyor <abdukhashimov@yandex.ru>
2020-04-17 20:17:03 +0500

    removed the commebts to improve readiblity

--- assignment_4_task3.py
+++ assignment_4_task3.py
@@ -7,8 +7,7 @@ ID: U1610131
 Name: Madiyor Abdukhashimov
 """
 
-# importing opencv library - to be able to import opencv you need to install
-# it using pip. The process of installation is provided in [README.md](../README.md)
+
 import cv2
 # importing numpy as np - which helps to generate matrices
 import numpy as np
